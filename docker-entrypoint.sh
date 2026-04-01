9207ea4fe7178e6a50efbd8b97a958698ff6c67c
Burrough Osborne <burrough@osbo.net>
2026-03-31 23:08:02 -0500

    Fix entrypoint to give webserver access to db folder

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 service cron start
 
+chown www-data:www-data /var/www/html/db
+
 # Install Composer dependencies on first start (vendor is not committed)
 if [ ! -d /var/www/html/vendor/phpmailer ]; then
     cd /var/www/html && composer install --no-interaction --quiet
